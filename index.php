2c1ebce318ccb0795d4296098e77bcef3b49e4c3
PiyushChoudhry <142146271+PiyushChoudhry@users.noreply.github.com>
2024-04-19 16:41:42 +0530

    index.php

--- index.php
+++ index.php
@@ -5,8 +5,7 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-	<title> "TicketEase"
- </title>
+	<title> TicketEase! </title>
 	<link rel="shortcut icon" href="images/favicon.png"></link>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -64,11 +63,11 @@ session_start();
 		<div class="navbar navbar-inverse">
 			<div class="navbar-inner">
 				<div class="container" >
-					<a class="brand" href="index.php" >HOME</a>
-					<a class="brand" href="train.php" >FIND TRAIN</a>
-					<a class="brand" href="reservation.php">RESERVATION</a>
-					<a class="brand" href="profile.php">PROFILE</a>
-					<a class="brand" href="booking.php">BOOKING HISTORY</a>
+				<a class="brand" href="index.php" >HOME</a>
+				<a class="brand" href="train.php" >FIND TRAIN</a>
+				<a class="brand" href="reservation.php">RESERVATION</a>
+				<a class="brand" href="profile.php">PROFILE</a>
+				<a class="brand" href="booking.php">BOOKING HISTORY</a>
 				</div>
 			</div>
 		</div>
@@ -76,19 +75,19 @@ session_start();
 			<!-- Photos slider -->
 			<div id="myCarousel" class="carousel slide" style="width:600px; float:left;margin-bottom:3px;">		
 				<div class="carousel-inner">
-					<div class="active item"><img src="images/modi.png" style="width:600px;height:350px;"/></div>
-					<div class="item"><img src="images/train1.avif" style="width:600px;height:350px;"/> </div>
-					<div class="item"><img src="images/train2.avif" style="width:600px;height:350px;"/></div>
-					<!-- <div class="item"><img src="images/9.jpg" style="width:600px;height:350px;"/></div>
-					<div class="item"><img src="images/10.jpg" style="width:600px;height:350px;"/> </div>
-					<div class="item"><img src="images/11.png"style="width:600px;height:350px;"/></div>
-					 -->
+				<div class="active item"><img src="images/modi.png" style="width:600px;height:350px;"/></div>
+				<div class="item"><img src="images/train1.avif" style="width:600px;height:350px;"/> </div>
+				<div class="item"><img src="images/train2.avif" style="width:600px;height:350px;"/></div>
+				<!-- <div class="item"><img src="images/9.jpg" style="width:600px;height:350px;"/></div>
+				<div class="item"><img src="images/10.jpg" style="width:600px;height:350px;"/> </div>
+				<div class="item"><img src="images/11.png"style="width:600px;height:350px;"/></div>
+				 -->
 				</div>
 				<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
 				<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
 			</div>
 			<!-- News and Alert-->
-			<div class="news" Style="float:right;">
+			<div class="news" Style="height:320px;float:right;">
     <marquee behavior="scroll" id="marq" scrollamount=3 direction="up" height="294px" onmouseover="nestop()" onmouseout="nestrt()">
         <div>
             <p><b>Vande Bharat train is running on time.</b></p></br>
@@ -101,17 +100,18 @@ session_start();
         </div>
     </marquee>
 </div>
-
+</div>
 		<!-- Copyright -->
 <footer >
 		<div style="width:100%;">
 			<div style="float:right;">
-				<p class="text-right text-info">Desinged By : Nitesh, Piyush & Nikhil</p>
-				<p class="text-right text-info">  &copy; 2024 Copyright PVT Ltd.</p>	
+			<p class="text-right text-info">  &copy; 2024 Copyright PVT Ltd.</p>	
+			</div>
+			<div style="float:left;">
+			<p class="text-left text-info">	Desinged By : Nitesh, Piyush & Nikhil</p>
 			</div>
 		</div>
-</footer>	
-</div>
+		</footer>
 	
 </body>
 </html>
